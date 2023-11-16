a6af9c36c3213ece0c075c1a12e74a8ed9cfb138
HoangViet <hoangphamhl000@gmail.com>
2023-11-16 17:05:00 +0700

    fix

--- src/services/userService.js
+++ src/services/userService.js
@@ -166,7 +166,7 @@ let deleteUsers = (userId) => {
 let editUsers = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     errMessage: 'Missing required parameters!'
@@ -181,6 +181,11 @@ let editUsers = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phonenumber = data.phonenumber;
+
                 await user.save();
 
                 resolve({
