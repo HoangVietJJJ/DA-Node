a07cfbf491ff2a160b70c740c13ecd9187f0d630
HoangViet <hoangphamhl000@gmail.com>
2023-11-18 17:22:15 +0700

    update doctor section

--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,7 @@
 import express from "express";
 import homeController from "../controllers/homeController";
 import userController from "../controllers/usercontroller";
+import doctorController from "../controllers/doctorController";
 
 let router = express.Router();
 
@@ -22,6 +23,8 @@ let initWebRoutes = (app) => {
     router.delete('/api/delete-users', userController.handleDeleteUser);
 
     router.get('/api/allcode', userController.getAllCode);
+
+    router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
 
     return app.use("/", router)
 }
