ea6fc8f835527a05258f4152462e57d8a423ec23
HoangViet <hoangphamhl000@gmail.com>
2023-11-22 16:57:36 +0700

    update

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -55,9 +55,23 @@ let getDetailDoctorById = async (req, res) => {
     }
 }
 
+let bulkCreateSchedule = async (req, res) => {
+    try {
+        let scheduleInfo = await doctorService.bulkCreateSchedule(req.body);
+        return res.status(200).json(scheduleInfo)
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server!',
+        })
+    }
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     postDoctorInfo: postDoctorInfo,
     getDetailDoctorById: getDetailDoctorById,
+    bulkCreateSchedule: bulkCreateSchedule,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -27,7 +27,8 @@ let initWebRoutes = (app) => {
     router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
     router.get('/api/get-all-doctors', doctorController.getAllDoctors);
     router.post('/api/save-doctors-info', doctorController.postDoctorInfo);
-    router.get('/api/get-details-doctor-by-id', doctorController.getDetailDoctorById)
+    router.get('/api/get-details-doctor-by-id', doctorController.getDetailDoctorById);
+    router.post('/api/bulk-create-schedule', doctorController.bulkCreateSchedule);
 
     return app.use("/", router)
 }
