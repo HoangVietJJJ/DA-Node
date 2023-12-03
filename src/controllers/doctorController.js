e51528b2821e896a64dfee960e247af92c706be9
HoangViet <hoangphamhl000@gmail.com>
2023-12-03 13:51:47 +0700

    update

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -94,6 +94,19 @@ let getExtraInforDoctorById = async (req, res) => {
     }
 }
 
+let getProfileDoctorById = async (req, res) => {
+    try {
+        let profile = await doctorService.getProfileDoctorById(req.query.doctorId);
+        return res.status(200).json(profile)
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server!',
+        })
+    }
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
@@ -101,5 +114,6 @@ module.exports = {
     getDetailDoctorById: getDetailDoctorById,
     bulkCreateSchedule: bulkCreateSchedule,
     getScheduleByDate: getScheduleByDate,
-    getExtraInforDoctorById: getExtraInforDoctorById
+    getExtraInforDoctorById: getExtraInforDoctorById,
+    getProfileDoctorById: getProfileDoctorById
 }
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -320,6 +320,68 @@ let getExtraInforDoctorById = (idInput) => {
     })
 }
 
+let getProfileDoctorById = (inputId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters!'
+                })
+            } else {
+                let dataDetail = await db.User.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: {
+                        exclude: ['password']
+                    },
+                    include: [
+                        {
+                            model: db.Markdown, attributes: ['description', 'contentHTML', 'contentMarkdown']
+                        },
+                        {
+                            model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi']
+                        },
+                        {
+                            model: db.Doctor_Infor,
+                            attributes: {
+                                exclude: ['id', 'doctorId',]
+                            },
+                            include: [
+                                {
+                                    model: db.Allcode, as: 'priceTypeData', attributes: ['valueEn', 'valueVi']
+                                },
+                                {
+                                    model: db.Allcode, as: 'provinceTypeData', attributes: ['valueEn', 'valueVi']
+                                },
+                                {
+                                    model: db.Allcode, as: 'paymentTypeData', attributes: ['valueEn', 'valueVi']
+                                }
+                            ],
+                        },
+                    ],
+                    raw: false,
+                    nest: true
+                })
+
+                if (dataDetail && dataDetail.image) {
+                    dataDetail.image = new Buffer(dataDetail.image, 'base64').toString('binary');
+                }
+
+                if (!dataDetail) dataDetail = {};
+
+                resolve({
+                    errCode: 0,
+                    data: dataDetail,
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
@@ -328,4 +390,5 @@ module.exports = {
     bulkCreateSchedule: bulkCreateSchedule,
     getScheduleByDate: getScheduleByDate,
     getExtraInforDoctorById: getExtraInforDoctorById,
+    getProfileDoctorById: getProfileDoctorById,
 }
